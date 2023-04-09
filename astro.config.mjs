4e809c966e94339491e2f62b85fb997d7c4c85ea
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2023-04-10 00:06:07 +0200

    cambio layaout.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://fidelChoqueDev.github.io',
-  base: '/magic-astro', 
+  base: '/magic-astro' 
 });
