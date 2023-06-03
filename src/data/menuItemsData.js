145f1ed15b8f0a1d7bd5afcacbeb42f4e4eaf17a
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2023-06-03 19:42:10 +0200

    add MenuNavBar

---
+++ src/data/menuItemsData.js
@@ -0,0 +1,29 @@
+// se procede a crear un arreglo de objetos para el menu de la pagina, dando la opcion de aumentar o disminuir opciones del menu
+const menuItems = [
+  {
+    title: 'Home',
+    url:'/'
+  },
+  {
+    title: 'About',
+    url:'/about'
+  },
+  {
+    title: 'Menu',
+    url:'/menu'
+  },
+  {
+    title: 'Order Online',
+    url:'/order'
+  },
+  {
+    title: 'Contact',
+    url:'/contact'
+  },
+  {
+    title: 'Blog',
+    url:'/blog'
+  },
+];
+
+export default menuItems;
